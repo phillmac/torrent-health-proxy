f9b555727a51590160f138d9c532de3fc68c0a4b
phillmac <phillmac@users.noreply.github.com>
2019-12-24 18:34:42 +1100

    Add initial files

---
+++ index.js
@@ -0,0 +1,19 @@
+if (!process.env.REDIS_HOST) {
+  throw Error('REDIS_HOST is required')
+}
+
+if (!process.env.REDIS_PORT) {
+  throw Error('REDIS_PORT is required')
+}
+
+const asyncRedis = require('async-redis')
+const http = require('http')
+
+const redisClient = asyncRedis.createClient({ host: process.env.REDIS_HOST, port: parseInt(process.env.REDIS_PORT) })
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  const raw = redisClient.hgetall('torrents')
+  const torrents = raw.map(t => JSON.parse(t))
+  res.end(JSON.stringify(torrents))
+}).listen(3001)
